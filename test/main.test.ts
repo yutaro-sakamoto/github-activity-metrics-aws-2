cfc6cc973c190b50af91a4759ffc553eab902070
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-04-23 01:34:16 +0900

    test: verify that S3 buckets are private

--- test/main.test.ts
+++ test/main.test.ts
@@ -14,3 +14,24 @@ const template = Template.fromStack(stack);
 test("Snapshot", () => {
   expect(template.toJSON()).toMatchSnapshot();
 });
+
+describe("S3 Bucket Public Access Tests", () => {
+  test("All S3 buckets should have block public access enabled", () => {
+    const bucketResources = template.findResources("AWS::S3::Bucket");
+    if (Object.keys(bucketResources).length > 0) {
+      Object.entries(bucketResources).forEach(([_logicalId, resource]) => {
+        const bucketProps = resource.Properties;
+
+        expect(bucketProps.PublicAccessBlockConfiguration).toBeDefined();
+
+        const publicAccessBlock = bucketProps.PublicAccessBlockConfiguration;
+        expect(publicAccessBlock.BlockPublicAcls).toBe(true);
+        expect(publicAccessBlock.BlockPublicPolicy).toBe(true);
+        expect(publicAccessBlock.IgnorePublicAcls).toBe(true);
+        expect(publicAccessBlock.RestrictPublicBuckets).toBe(true);
+      });
+
+      expect(Object.keys(bucketResources).length).toBeGreaterThan(0);
+    }
+  });
+});
