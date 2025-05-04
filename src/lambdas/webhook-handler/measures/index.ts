f1af04b3dd86062db554a7df622f2c1408bcd299
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-05-04 17:00:08 +0900

    refactor: add a type definition of measures

--- src/lambdas/webhook-handler/measures/index.ts
+++ src/lambdas/webhook-handler/measures/index.ts
@@ -2,13 +2,45 @@
  * GitHub イベントタイプに基づいて Timestream のメジャー定義を返す
  */
 
+// The definition of measureType is based on the Timestream data types.
+// See https://docs.aws.amazon.com/ja_jp/timestream/latest/developerguide/writes.html#writes.data-types for detail.
+
+type measureTypeAtom =
+  | {
+      measureName: string;
+      measureValueType: "BIGINT" | "DOUBLE";
+      measureValue: number;
+    }
+  | {
+      measureName: string;
+      measureValueType: "VARCHAR";
+      measureValue: string;
+    }
+  | {
+      measureName: string;
+      measureValueType: "BOOLEAN";
+      measureValue: boolean;
+    };
+
+type measureType =
+  | measureTypeAtom
+  | {
+      measureName: string;
+      measureValueType: "MULTI";
+      measureValues: {
+        Name: string;
+        Type: "BIGINT" | "DOUBLE" | "VARCHAR" | "BOOLEAN";
+        Value: string;
+      }[];
+    };
+
 /**
  * イベントタイプに応じたメジャー定義を取得する
  * @param event_type GitHubイベントタイプ
  * @param payload GitHubイベントのペイロードデータ
  * @returns Timestreamメジャー定義
  */
-export function getMeasure(event_type: string, payload: any): any {
+export function getMeasure(event_type: string, payload: any): measureType {
   switch (event_type) {
     case "push": {
       return {
