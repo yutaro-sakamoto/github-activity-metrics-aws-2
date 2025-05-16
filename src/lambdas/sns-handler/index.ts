b0060b016cf4df2a3db85e58d125ac1c5354e5ac
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-05-17 01:13:06 +0900

    add: invoke GitHub API

--- src/lambdas/sns-handler/index.ts
+++ src/lambdas/sns-handler/index.ts
@@ -1,4 +1,5 @@
 import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+//import { Octokit } from "@octokit/rest";
 const ssmClient = new SSMClient();
 
 /**
@@ -9,21 +10,37 @@ const ssmClient = new SSMClient();
 export const handler = async (event: any, context: any) => {
   console.log("event:", JSON.stringify(event));
   console.log("context:", JSON.stringify(context));
-
-  const secretToken = await getSecretFromParameterStore(
-    "/github/metrics/github-token",
-  );
-
-  console.log("Length of secretToken:", secretToken.length);
+  const { Octokit } = await import("@octokit/rest");
 
   // SNSメッセージの処理
   try {
     // SNSイベントからメッセージを取得
     const records = event.Records || [];
+    const secretToken = await getSecretFromParameterStore(
+      "/github/metrics/github-token",
+    );
+
+    console.log("Length of secretToken:", secretToken.length);
     for (const record of records) {
       if (record.Sns) {
         const message = record.Sns.Message;
         console.log("SNS message:", message);
+
+        const parsedMessage = JSON.parse(message);
+
+        const octokit = new Octokit({
+          auth: secretToken,
+        });
+
+        const { data: pullRequest } = await octokit.pulls.get({
+          owner: parsedMessage.organization,
+          repo: parsedMessage.repository,
+          pull_number: parsedMessage.number,
+        });
+
+        console.log("changed_files:", pullRequest.changed_files);
+        console.log("additions:", pullRequest.additions);
+        console.log("deletions:", pullRequest.deletions);
 
         // ここで必要な処理を追加できます
         // このシンプルな例では、メッセージを出力するだけです
